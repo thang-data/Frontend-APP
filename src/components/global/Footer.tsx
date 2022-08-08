7b1d2d3b25ea70365307290f10e4256980ec5234
Nguyen Duc Thang <thang-nd@blockhive.vn>
2022-08-08 13:35:49 +0700

    Update Footer.tsx

--- src/components/global/Footer.tsx
+++ src/components/global/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="text-center bg-light py-4">
       <h6>Chào mừng đến Law Blog. Trang Blog của ngành Luật</h6>
-      <p> Copyright &copy; 2021</p>
+      <p> Copyright &copy; 2022</p>
     </div>
   )
 }
